4597d2f8ce998a58047752e0f076607cd5f1d682
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-01-12 16:44:40 +0300

    install.sh init

---
+++ install.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autoupdate-oh-my-zsh-plugins
+
+git clone https://github.com/Aloxaf/fzf-tab.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
+
+git clone https://github.com/zpm-zsh/ssh.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/ssh
